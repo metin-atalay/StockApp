99ab0c40ea2336d3a86f5cf341a2cf1941d4face
Metin ATALAY <metin.atalay@architecht.com>
2022-02-13 15:45:30 +0300

    Initial Commit

---
+++ StocksApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StocksApp
+//
+//  Created by Metin Atalay on 13.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by Metin Atalay on 13.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

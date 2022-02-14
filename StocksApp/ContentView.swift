7a3122eeda8dee8bde80065ce690f3e67eb64269
Metin ATALAY <metin.atalay@architecht.com>
2022-02-14 15:22:14 +0300

    code was added

--- StocksApp/ContentView.swift
+++ StocksApp/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @ObservedObject private var stockListVM = StockListViewModel()
+    
+    init() {
+        UINavigationBar.appearance().backgroundColor = UIColor.black
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        UITableView.appearance().backgroundColor = UIColor.black
+        UITableViewCell.appearance().backgroundColor = UIColor.black
+        
+        stockListVM.load()
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        let filteredStocks = self.stockListVM.searchTerm.isEmpty ? self.stockListVM.stocks : self.stockListVM.stocks.filter { $0.symbol.starts(with: self.stockListVM.searchTerm ) }
+        
+        return NavigationView {
+            
+            ZStack(alignment: .leading) {
+                
+                Color.black
+                
+                Text("12 FEB 2022")
+                    .font(.custom("Arial", size: 32))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.gray)
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                    .offset(y: -400)
+                
+                SearchView(searchTerm: self.$stockListVM.searchTerm)
+                    .offset(y: -300)
+                
+                StockListView(stocks: filteredStocks)
+                    .offset(y: 75)
+                
+            }
+            
+            
+            .navigationBarTitle("Stocks")
+        }.edgesIgnoringSafeArea(Edge.Set(.bottom))
     }
 }
 
@@ -18,4 +55,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+
 }
--- StocksApp/Services/Webservice.swift
+++ StocksApp/Services/Webservice.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+
+class Webservice {
+    
+    func getStocks(completion: @escaping(([Stock]?)-> Void)) {
+        
+        guard let url = URL(string: "https://island-bramble.glitch.me/stocks") else {
+            completion(nil)
+            return
+            
+        }
+        
+       
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+
+            }
+            
+            let stocks = try? JSONDecoder().decode([Stock].self, from: data)
+            stocks == nil ? completion(nil): completion(stocks)
+            
+           
+        }.resume()
+        
+    }
+    
+}
